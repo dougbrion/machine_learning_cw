7bbe4d60bfa538ccbb26a82244617d2e61d0e329
Doug Brion <db1415@ic.ac.uk>
2018-03-13 14:04:16 +0000

    Added report and gitignore

--- code/logistic_regression.py
+++ code/logistic_regression.py
@@ -4,6 +4,7 @@ import helpers as hp
 
 def calc_error(_X, _y, _W, _b):
     pred = tf.add(tf.matmul(_X, _W), _b)
+    # cost = tf.reduce_mean(tf.reduce_sum(tf.matmul(tf.transpose(_y), tf.log(pred)), reduction_indices=1))
     cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=pred, labels=_y))
     return pred, cost
 
@@ -39,8 +40,8 @@ def logistic_regression(_train_X, _train_y, _epochs, _rate):
                 x_axis.append(epoch + 1)
                 y_axis.append(c)
 
-            # if (epoch + 1) % 50 == 0:
-                #print("Epoch:", '%04d' % (epoch + 1), "cost=", c)
+            if (epoch + 1) % 50 == 0:
+                print("Epoch:", '%04d' % (epoch + 1), "cost=", c)
             
         #print("Optimization Finished!")
         training_cost = sess.run(cost, feed_dict={X: _train_X, y: _train_y})
